d563ea58933b8f0bfb2638af7ffcb210fd85ba44
Paulius <a.paulius@gmail.com>
2017-03-16 17:52:15 +0200

    add sysout The World is not ugly

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ package firstlesson;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!"),
+		System.out.println("HELLO UGLY WORLD!");
+		System.out.println("The World is not ugly!");
 	}
 }
