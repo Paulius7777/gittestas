421dd14fe486f0d6edc4cfbab75994e12dbc83cf
Paulius <a.paulius@gmail.com>
2017-03-16 18:44:16 +0200

    rredaguotas hello world failas su readeriu

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+
 package firstlesson;
 
 /**
@@ -6,8 +7,12 @@ package firstlesson;
  */
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!");
-		System.out.println("The World is not ugly!");
+		
+		Input ivestis = new Input();
+		System.out.println("Iveskite savo varda");
+		String name = ivestis.ivestis();
+		System.out.println("Hello "+ name);
+	        }
 	}
-}
