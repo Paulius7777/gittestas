597009c71862e8503e0acadd6819c7f33403605b
Paulius <a.paulius@gmail.com>
2017-03-16 18:51:20 +0200

    add to HelloWorld date time

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,17 @@ package firstlesson;
  *
  */
 
+import java.time.LocalDate;
+
 public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!");
-		System.out.println("The World is not ugly!");
+		
+		Input ivestis = new Input();
+		System.out.println("Iveskite savo varda");
+		String name = ivestis.ivestis();
+		System.out.println("Hello "+ name);
+		LocalDate date = LocalDate.now();
+		System.out.println("date");
+	        }
 	}
-}
